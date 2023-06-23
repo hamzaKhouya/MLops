a95f9f3973f34cf2337265ffa9f863ea88f4f05f
chaimaehj <hajjaji.ch@gmail.com>
2023-06-23 10:41:45 +0100

    Test d'affichage

--- TestAff.py
+++ TestAff.py
@@ -6,22 +6,19 @@ import dash
 from dash import dcc, html
 import plotly.graph_objs as go
 import os
+import sys
 
 # Device configuration
 device = torch.device("cpu")
 
 
 def generate(name):
-    # If you what to replicate the DeepLog paper results(Actually, I have a better result than DeepLog paper results),
-    # you should use the 'list' not 'set' to obtain the full dataset, I use 'set' just for test and acceleration.
     hdfs = set()
-    # hdfs = []
     with open(os.path.join('C:\\Users\\Chaimae\\Desktop\\MLops\\data', name), 'r') as f:
         for ln in f.readlines():
             ln = list(map(lambda n: n - 1, map(int, ln.strip().split())))
             ln = ln + [-1] * (window_size + 1 - len(ln))
             hdfs.add(tuple(ln))
-            # hdfs.append(tuple(ln))
     print('Number of sessions({}): {}'.format(name, len(hdfs)))
     return hdfs
 
@@ -102,56 +99,13 @@ if __name__ == '__main__':
     F1 = 2 * P * R / (P + R)
     print('false positive (FP): {}, false negative (FN): {}, Precision: {:.3f}%, Recall: {:.3f}%, F1-measure: {:.3f}%'.format(FP, FN, P, R, F1))
     print('Finished Predicting')
-    
-    # Data for bar chart
-    PCA = (0.98, 0.67, 0.79)
-    LSTM = (0.9526, 0.9903, 0.9711)
-    x = [8, 9, 10, 11]
-    FP = [605, 588, 495, 860]
-    FN = [465, 333, 108, 237]
-    TP = [4123 - fn for fn in FN]
-    P = [tp / (tp + fp) for tp, fp in zip(TP, FP)]
-    R = [tp / (tp + fn) for tp, fn in zip(TP, FN)]
-    F1 = [2 * p * r / (p + r) for p, r in zip(P, R)]
-    
+
     # Create bar chart trace
     trace1 = go.Bar(
         x=['Precision', 'Recall', 'F1-score'],
-        y=PCA,
-        name='PCA',
+        y=[P/100, R/100, F1/100],
+        name='Your Model',
         marker=dict(color='blue', opacity=0.4)
-    )
-
-    trace2 = go.Bar(
-        x=['Precision', 'Recall', 'F1-score'],
-        y=LSTM,
-        name='LSTM',
-        marker=dict(color='red', opacity=0.4)
-    )
-
-    # Create line chart trace
-    trace3 = go.Scatter(
-        x=x,
-        y=P,
-        mode='lines',
-        name='Precision',
-        line=dict(color='red', dash='dot')
-    )
-
-    trace4 = go.Scatter(
-        x=x,
-        y=R,
-        mode='lines',
-        name='Recall',
-        line=dict(color='blue', dash='dot')
-    )
-
-    trace5 = go.Scatter(
-        x=x,
-        y=F1,
-        mode='lines',
-        name='F1-score',
-        line=dict(color='black', dash='dot')
     )
 
     # Create Dash app and layout
@@ -162,7 +116,7 @@ if __name__ == '__main__':
             dcc.Graph(
                 id='bar-chart',
                 figure={
-                    'data': [trace1, trace2],
+                    'data': [trace1],
                     'layout': go.Layout(
                         title='Scores by different models',
                         xaxis=dict(title='Measure'),
@@ -171,24 +125,8 @@ if __name__ == '__main__':
                     )
                 }
             )
-        ]),
-        html.Div(children=[
-            dcc.Graph(
-                id='line-chart',
-                figure={
-                    'data': [trace3, trace4, trace5],
-                    'layout': go.Layout(
-                        title='Scores by window size',
-                        xaxis=dict(title='window_size'),
-                        yaxis=dict(title='scores'),
-                        legend=dict(
-                            x=0.7,
-                            y=1
-                        ),
-                        xaxis_tickvals=x,
-                        yaxis_range=[0.5, 1]
-                    )
-                }
-            )
         ])
     ])
+
+if __name__ == '__main__':
+    app.run_server(debug=False,host='0.0.0.0')
