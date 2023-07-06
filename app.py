69a80890f77ddafffdd4e71fbc406843180a72d3
Hamza Khouya <hamzakhouya5@gmail.com>
2023-07-06 19:26:50 +0100

    update files

--- app.py
+++ app.py
@@ -38,92 +38,91 @@ class Model(nn.Module):
         return out
 
 
-if __name__ == '__main__':
 
-    # Hyperparameters
-    num_classes = 28
-    input_size = 1
-    model_path = os.path.join(os.path.abspath('model/Adam_batch_size=256_epoch=5.pt'))
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-num_layers', default=2, type=int)
-    parser.add_argument('-hidden_size', default=64, type=int)
-    parser.add_argument('-window_size', default=10, type=int)
-    parser.add_argument('-num_candidates', default=9, type=int)
-    args = parser.parse_args()
-    num_layers = args.num_layers
-    hidden_size = args.hidden_size
-    window_size = args.window_size
-    num_candidates = args.num_candidates
+# Hyperparameters
+num_classes = 28
+input_size = 1
+model_path = os.path.join(os.path.abspath('model/Adam_batch_size=256_epoch=5.pt'))
+parser = argparse.ArgumentParser()
+parser.add_argument('-num_layers', default=2, type=int)
+parser.add_argument('-hidden_size', default=64, type=int)
+parser.add_argument('-window_size', default=10, type=int)
+parser.add_argument('-num_candidates', default=9, type=int)
+args = parser.parse_args()
+num_layers = args.num_layers
+hidden_size = args.hidden_size
+window_size = args.window_size
+num_candidates = args.num_candidates
 
-    model = Model(input_size, hidden_size, num_layers, num_classes).to(device)
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    print('model_path: {}'.format(model_path))
-    test_normal_loader = generate('hdfs_test_normal')
-    test_abnormal_loader = generate('hdfs_test_abnormal')
-    TP = 0
-    FP = 0
-    # Test the model
-    start_time = time.time()
-    with torch.no_grad():
-        for line in test_normal_loader:
-            for i in range(len(line) - window_size):
-                seq = line[i:i + window_size]
-                label = line[i + window_size]
-                seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
-                label = torch.tensor(label).view(-1).to(device)
-                output = model(seq)
-                predicted = torch.argsort(output, 1)[0][-num_candidates:]
-                if label not in predicted:
-                    FP += 1
-                    break
-    with torch.no_grad():
-        for line in test_abnormal_loader:
-            for i in range(len(line) - window_size):
-                seq = line[i:i + window_size]
-                label = line[i + window_size]
-                seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
-                label = torch.tensor(label).view(-1).to(device)
-                output = model(seq)
-                predicted = torch.argsort(output, 1)[0][-num_candidates:]
-                if label not in predicted:
-                    TP += 1
-                    break
-    elapsed_time = time.time() - start_time
-    print('elapsed_time: {:.3f}s'.format(elapsed_time))
-    # Compute precision, recall and F1-measure
-    FN = len(test_abnormal_loader) - TP
-    P = 100 * TP / (TP + FP)
-    R = 100 * TP / (TP + FN)
-    F1 = 2 * P * R / (P + R)
+model = Model(input_size, hidden_size, num_layers, num_classes).to(device)
+model.load_state_dict(torch.load(model_path))
+model.eval()
+print('model_path: {}'.format(model_path))
+test_normal_loader = generate('hdfs_test_normal')
+test_abnormal_loader = generate('hdfs_test_abnormal')
+TP = 0
+FP = 0
+# Test the model
+start_time = time.time()
+with torch.no_grad():
+    for line in test_normal_loader:
+        for i in range(len(line) - window_size):
+            seq = line[i:i + window_size]
+            label = line[i + window_size]
+            seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+            label = torch.tensor(label).view(-1).to(device)
+            output = model(seq)
+            predicted = torch.argsort(output, 1)[0][-num_candidates:]
+            if label not in predicted:
+                FP += 1
+                break
+with torch.no_grad():
+    for line in test_abnormal_loader:
+        for i in range(len(line) - window_size):
+            seq = line[i:i + window_size]
+            label = line[i + window_size]
+            seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+            label = torch.tensor(label).view(-1).to(device)
+            output = model(seq)
+            predicted = torch.argsort(output, 1)[0][-num_candidates:]
+            if label not in predicted:
+                TP += 1
+                break
+elapsed_time = time.time() - start_time
+print('elapsed_time: {:.3f}s'.format(elapsed_time))
+# Compute precision, recall and F1-measure
+FN = len(test_abnormal_loader) - TP
+P = 100 * TP / (TP + FP)
+R = 100 * TP / (TP + FN)
+F1 = 2 * P * R / (P + R)
 
-    # Create bar chart trace
-    trace1 = go.Bar(
-        x=['Precision', 'Recall', 'F1-score'],
-        y=[P/100, R/100, F1/100],
-        name='Your Model',
-        marker=dict(color='blue', opacity=0.4)
-    )
+# Create bar chart trace
+trace1 = go.Bar(
+    x=['Precision', 'Recall', 'F1-score'],
+    y=[P/100, R/100, F1/100],
+    name='Your Model',
+    marker=dict(color='blue', opacity=0.4)
+)
 
-    # Create Dash app and layout
-    app = dash.Dash(__name__)
-    app.layout = html.Div(children=[
-        html.H1(children='Scores by different models'),
-        html.Div(children=[
-            dcc.Graph(
-                id='bar-chart',
-                figure={
-                    'data': [trace1],
-                    'layout': go.Layout(
-                        title='Scores by different models',
-                        xaxis=dict(title='Measure'),
-                        yaxis=dict(title='Scores'),
-                        barmode='group'
-                    )
-                }
-            )
-        ])
+# Create Dash app and layout
+app = dash.Dash(__name__)
+app.layout = html.Div(children=[
+    html.H1(children='Scores by different models'),
+    html.Div(children=[
+        dcc.Graph(
+            id='bar-chart',
+            figure={
+                'data': [trace1],
+                'layout': go.Layout(
+                    title='Scores by different models',
+                    xaxis=dict(title='Measure'),
+                    yaxis=dict(title='Scores'),
+                    barmode='group'
+                )
+            }
+        )
     ])
+])
 
 """ if __name__ == '__main__':
     app.run_server(debug=False,host='0.0.0.0')
