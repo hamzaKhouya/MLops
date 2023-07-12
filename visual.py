b417c6524272f633e02bef744553d2c3b941a3d5
Hamza Khouya <hamzakhouya5@gmail.com>
2023-07-12 20:03:42 +0100

    new up

--- visual.py
+++ visual.py
@@ -1,99 +1,77 @@
-import numpy as np
 import dash
-from dash import html, dcc
-import plotly.graph_objs as go
+from dash import html, dcc, Input, Output
+#import plotly.graph_objs as go
+import dash_bootstrap_components as dbc
+import plotly.express as px
 
-# Data for bar chart
-PCA = (0.98, 0.67, 0.79)
-LSTM = (0.9526, 0.9903, 0.9711)
-
-# Data for line chart
-x = [8, 9, 10, 11]
-FP = [605, 588, 495, 860]
-FN = [465, 333, 108, 237]
+x = [20,10, 5,0]
+FP = [234, 400, 600, 970]
+FN = [40, 103, 200, 500]
 TP = [4123 - fn for fn in FN]
 P = [tp / (tp + fp) for tp, fp in zip(TP, FP)]
 R = [tp / (tp + fn) for tp, fn in zip(TP, FN)]
 F1 = [2 * p * r / (p + r) for p, r in zip(P, R)]
 
-# Create bar chart trace
-trace1 = go.Bar(
-    x=['Precision', 'Recall', 'F1-score'],
-    y=PCA,
-    name='PCA',
-    marker=dict(color='blue', opacity=0.4)
+LOGO = "https://cd.foundation/wp-content/uploads/sites/78/2020/05/mlopscard.png"
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Col(html.Img(src=LOGO, height="100px",
+                             className="float-start")),
+            dbc.Collapse(
+                [
+                    dbc.NavItem(dbc.NavLink("Differents metrics", href="/",
+                                            active="exact", className="btn btn-outline-info m-1")),
+                ],
+                id="navbar-collapse",
+                is_open=False,
+                navbar=True,
+            ),
+            dbc.Col(
+            dcc.Dropdown(
+                id="metric",
+                options=["Precision","Recall","F1-Score"],
+                multi=True,
+                placeholder="Metrics",
+                style = {'float' : 'left' , 'width' : '300px' }
+            ),
+            className="mt-1"
+        )
+        ], fluid=True
+    ),
+    className="my-1",
+    dark=True,
 )
 
-trace2 = go.Bar(
-    x=['Precision', 'Recall', 'F1-score'],
-    y=LSTM,
-    name='LSTM',
-    marker=dict(color='red', opacity=0.4)
-)
-
-# Create line chart trace
-trace3 = go.Scatter(
-    x=x,
-    y=P,
-    mode='lines',
-    name='Precision',
-    line=dict(color='red', dash='dot')
-)
-
-trace4 = go.Scatter(
-    x=x,
-    y=R,
-    mode='lines',
-    name='Recall',
-    line=dict(color='blue', dash='dot')
-)
-
-trace5 = go.Scatter(
-    x=x,
-    y=F1,
-    mode='lines',
-    name='F1-score',
-    line=dict(color='black', dash='dot')
-)
+figures = dbc.Row([
+                 dbc.Col(dcc.Graph(id = 'precision'),width={'size':4 , "order": 1}, 
+                                 style = {'border-style': 'solid', 'border-color': 'gray'}),
+                 dbc.Col(dcc.Graph(id = 'recall'),width={'size': 4, "order": 2},
+                              style = {'border-style': 'solid', 'border-color': 'gray'}),
+                 dbc.Col(dcc.Graph(id = 'f1-score'),width={'size': 4, "order": 2},
+                              style = {'border-style': 'solid', 'border-color': 'gray'})])
 
 # Create Dash app and layout
-app = dash.Dash(__name__)
-app.layout = html.Div(children=[
-    html.H1(children='Scores by different models'),
-    html.Div(children=[
-        dcc.Graph(
-            id='bar-chart',
-            figure={
-                'data': [trace1, trace2],
-                'layout': go.Layout(
-                    title='Scores by different models',
-                    xaxis=dict(title='Measure'),
-                    yaxis=dict(title='Scores'),
-                    barmode='group'
-                )
-            }
-        )
-    ]),
-    html.Div(children=[
-        dcc.Graph(
-            id='line-chart',
-            figure={
-                'data': [trace3, trace4, trace5],
-                'layout': go.Layout(
-                    title='Scores by window size',
-                    xaxis=dict(title='window_size'),
-                    yaxis=dict(title='scores'),
-                    legend=dict(
-                        x=0.7,
-                        y=1
-                    ),
-                    xaxis_tickvals=x,
-                    yaxis_range=[0.5, 1]
-                )
-            }
-        )
-    ])
-])
+FONT_AWESOME = "https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, FONT_AWESOME])
 
+app.layout = html.Div(children=[navbar, figures])
+
+@app.callback(
+    [Output('precision', 'figure'),
+     Output('recall', 'figure'),
+     Output('f1-score', 'figure'),
+     Input('metric' ,'value')
+     ])
+def update_graph(selected_metric):
+    
+    precision = px.line(x=x,y=P)
+ 
+    recall = px.line(x=x,y=R)
+    
+    f1_score = px.line(x=x,y=F1)
+                            
+    return precision, recall, f1_score
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
